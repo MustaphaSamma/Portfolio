df36920a8b292c815ed261effe1cdd8ee70c0d32
Mustapha Samma <48632586+MustaphaSamma@users.noreply.github.com>
2022-10-18 15:21:43 +0100

    Revert "Update index.js"
    This reverts commit fd02335848b70e13ff556cd0fe36dd414f8aee9e.

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,15 +8,15 @@ const Footer = ({}) => {
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
-          <h1 className="text-2xl text-bold"></h1>
+          <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-            Let's keep in touch
+              LET&apos;S WORK
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              
+              TOGETHER
             </h1>
-            <Button type="primary">Contact Me</Button>
+            <Button type="primary">Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,9 +24,9 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" Mustapha Samma"}
-        <Link href="http://www.mustaphasamma.com">
-          <a className="underline underline-offset-1"></a>
+        Made With ❤ by{" "}
+        <Link href="http://www.chetanverma.com">
+          <a className="underline underline-offset-1">Chetan Verma</a>
         </Link>
       </h1>
     </>
