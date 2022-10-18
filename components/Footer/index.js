e5e8d4264b371015f1746a48c5d429ea3a9cc15f
Mustapha Samma <48632586+MustaphaSamma@users.noreply.github.com>
2022-10-18 14:56:01 +0100

    commit3

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,15 +8,15 @@ const Footer = ({}) => {
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
-          <h1 className="text-2xl text-bold">Contact.</h1>
+          <h1 className="text-2xl text-bold"></h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
+            Let's keep in touch
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
+              
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <Button type="primary">Contact Me</Button>
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,9 +24,9 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        Made With ❤ by{" Mustapha Samma"}
+        <Link href="http://www.mustaphasamma.com">
+          <a className="underline underline-offset-1"></a>
         </Link>
       </h1>
     </>
